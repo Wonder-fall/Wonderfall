bd0590126e54d18c98917762256a01a7a934219f
Bar David <BarDavid@Bar>
2015-11-25 13:04:21 +0200

    Init

--- core/src/com/wonderfall/game/WonderfallGame.java
+++ core/src/com/wonderfall/game/WonderfallGame.java
@@ -5,6 +5,13 @@ import com.wonderfall.game.screens.MainMenuScreen;
 import com.wonderfall.game.utils.Assets;
 import com.wonderfall.game.utils.LevelsManager;
 
+/**
+ * 
+ * Wonderfall game.
+ * 
+ * @version 1.0
+ *
+ */
 public class WonderfallGame extends Game {
 
 	@Override
